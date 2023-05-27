275dd19b9d1de66221c649a782751c3fe446a4c2
Mustafa Yanar <mustafayanar0007@gmail.com>
2023-05-27 16:58:41 +0300

    Constructed 3 Frequent Model

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
-
-# Write code to read a file named Frogs_MFCCs.csv in dataset folder
-# Path: main.py
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules, fpgrowth
 import pandas as pd
-df = pd.read_csv('dataset/Frogs_MFCCs.csv')
-print(df.head())
 
-#Print the first 5 rows of the dataset
+# Veri setini yükleyin
+data = data = pd.read_csv('dataset/Frogs_MFCCs.csv')
 
-df.sample(5)
+# MFCC özelliklerini alın
+features = data.columns[-4:]  # Son dört sütun Family, Genus, Species ve RecordID olduğunu varsayalım
+
+# Verileri uygun formata dönüştürün
+transactions = data[features].astype(str).values.tolist()
+
+# Verileri TransactionEncoder ile kodlayın
+te = TransactionEncoder()
+te_ary = te.fit(transactions).transform(transactions)
+df_encoded = pd.DataFrame(te_ary, columns=te.columns_)
+
+# Apriori algoritmasını uygulayın
+frequent_itemsets = apriori(df_encoded, min_support=0.1, use_colnames=True)
+
+# İlişkisel kuralları çıkarın
+rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.5)
+
+# Sonuçları görüntüle
+print("Sık Öğeler:")
+print(frequent_itemsets)
+print("\nİlişkisel Kurallar:")
+print(rules)
+
+# FP-Growth algoritmasını uygulayın
+frequent_itemsets_fp = fpgrowth(df_encoded, min_support=0.1, use_colnames=True)
+
+# İlişkisel kuralları çıkarın
+rules_fp = association_rules(frequent_itemsets_fp, metric="confidence", min_threshold=0.5)
+
+# Sonuçları görüntüle
+print("FP-Growth ile Sık Öğeler:")
+print(frequent_itemsets_fp)
+print("\nFP-Growth ile İlişkisel Kurallar:")
+print(rules_fp)
+
+# ECLAT algoritmasını uygulayın
+frequent_itemsets = apriori(df_encoded, min_support=0.1, use_colnames=True)
+
+# İlişkisel kuralları çıkarın
+rules = association_rules(frequent_itemsets,metric='support',min_threshold= 0.1)
+
+# Sonuçları görüntüle
+print("Sık Öğeler:")
+print(frequent_itemsets)
+print("\nİlişkisel Kurallar:")
+print(rules)
